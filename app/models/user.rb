c7d74a6ad6eb1bcef7253be77d48a77a5f7659bb
rohit-yawalkar <rohit.yawalkar@thoughtworks.com>
2021-11-07 18:38:29 +0530

    [Rohit] - Introduces scss and resource in app

---
+++ app/models/user.rb
@@ -0,0 +1,9 @@
+class User < ApplicationRecord
+  # bundle exec rails g model user name:string email:string password:digest
+  # using above command has_secure_command is already there, it can be used to decrypt the password and validate the user
+  # u = User.new
+  # u.name="Rohit" u.email="test@example.com" u.password="12345" u.password_confirmation="12345" u.save password_digest will be crypted one
+  # throws error if pass and pass_confirm doesn't matches
+  # u.authenticate("any_password") -> false i wrong, object if right password
+  has_secure_password
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,17 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  root "players#index"
-  get "players" => "players#index"
-  get "players/new" => "players#new"
-  post "players" => "players#create"
-  get "players/:id" => "players#show", as: "player"
-  get "players/:id/edit" => "players#edit", as: "player_edit"
-  patch "players/:id" => "players#update"
-  delete "players/:id" => "players#destroy"
+  root "users#index"
+
+  resources :users, only: [:index, :create]
+  resources :players
+
+  # below are 7 restfull routes
+
+  # get "players" => "players#index"
+  # get "players/new" => "players#new"
+  # post "players" => "players#create"
+  # get "players/:id" => "players#show", as: "player"
+  # get "players/:id/edit" => "players#edit", as: "player_edit"
+  # patch "players/:id" => "players#update"
+  # delete "players/:id" => "players#destroy"
 end
